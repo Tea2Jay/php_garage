d9a25e37e0fdac860e48525a446edfaa9746a88e
Tariq Aljaber <itrq222@gmail.com>
2020-06-04 20:41:36 +0300

    simple html with php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo 'hello world';
+<head>
+    <meta charset="UTF-8">
+    <title>Documnet</title>
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <h1><?="hello ". htmlspecialchars($_GET['key']); 
+            
+            ?>
+        </h1>
+    </header>
+</body>
+
+</html>
